57f361370b562943151c5b449c950318f229d970
Matheus <mtthew.providr@gmail.com>
2026-08-27 21:08:56 -0300

    exercise: solve 07-basic-math

---
+++ Study's/Module-01/07-basic-math.py
@@ -0,0 +1,9 @@
+#Create two number variables and print the result of adding, subtracting, multiplying, and dividing them.
+
+number1 = 5
+number2 = 8
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
