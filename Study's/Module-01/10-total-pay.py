b0f4c14e9fd3971499b796b4e03b7a3b7b2d1896
Matheus <mtthew.providr@gmail.com>
2026-08-27 21:15:45 -0300

    exercise: solve 10-total-pay

---
+++ Study's/Module-01/10-total-pay.py
@@ -0,0 +1,7 @@
+#Given hours_worked and hourly_rate variables, calculate and print the total pay.
+
+hours_worked = 12
+hourly_rate = 10
+total_pay = hours_worked * hourly_rate
+
+print(f"Total Pay: {total_pay}")
