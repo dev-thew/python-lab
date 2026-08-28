79e43b564c3f0c2823c3d1ec5dd28ffc710e484b
Matheus <mtthew.providr@gmail.com>
2026-08-28 09:09:17 -0300

    exercise: solve 29-comment-practice

---
+++ Study's/Module-01/29-comment-practice.py
@@ -0,0 +1,5 @@
+#Pegue qualquer exercício anterior e adicione um comentário # acima de cada linha explicando o que ela faz.
+
+number = float(input("Enter a Number: "))  # Solicita ao usuário que insira um número e converte para float
+value = abs(number)  # Calcula o valor absoluto do número inserido
+print(value)  # Imprime o valor absoluto
