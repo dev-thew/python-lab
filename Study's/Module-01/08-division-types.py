2473fb7fca00408f663d98bc2998f3d74d60172d
Matheus <mtthew.providr@gmail.com>
2026-08-27 21:11:20 -0300

    exercise: solve 08-division-types

---
+++ Study's/Module-01/08-division-types.py
@@ -0,0 +1,7 @@
+#Divide two integers using / and then using //. Print both results and explain the difference in a comment.
+
+number1 = 9
+number2 = 3
+
+print(number1 / number2)
+print(number1 // number2)
