653d20ed49d910141d86992d721260900060bb27
Matheus <mtthew.providr@gmail.com>
2026-08-28 08:28:35 -0300

    exercise: solve 19-input-float

---
+++ Study's/Module-01/19-input-float.py
@@ -0,0 +1,6 @@
+#Ask the user for a product price (as text) and print it converted to a float, along with its type().
+
+price = input("Enter the product price: ")
+price = float(price)
+print(price)
+print(type(price))
