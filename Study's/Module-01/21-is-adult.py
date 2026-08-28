e5ddb2f3fc6c78d3c03c01122cde18b8912d2c65
Matheus <mtthew.providr@gmail.com>
2026-08-28 08:40:35 -0300

    exercise: solve 21-is-adult

---
+++ Study's/Module-01/21-is-adult.py
@@ -0,0 +1,4 @@
+#Peça a idade do usuário (convertida para int) e imprima o resultado de compará-la com 18 usando >= — só o booleano, sem if ainda.
+
+age = int(input("Digite sua idade: "))
+print(age >= 18)
