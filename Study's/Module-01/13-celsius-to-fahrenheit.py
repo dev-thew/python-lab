9006e6e6cea4e617243d33d8fc044c1986b6d040
Matheus <mtthew.providr@gmail.com>
2026-08-27 21:30:34 -0300

    exercise: solve 13-celsius-to-fahrenheit

---
+++ Study's/Module-01/13-celsius-to-fahrenheit.py
@@ -0,0 +1,7 @@
+#Ask the user for a Celsius temperature and convert it to Fahrenheit.
+
+celsius = float(input("Quantity degrees Celsius: "))
+
+fahrenheit = (celsius * 9/5 + 32)
+
+print(f"Degrees Fahrenheit: {fahrenheit}")
