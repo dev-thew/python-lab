6a9c3292c200879bb8abd082c4796c50545f6586
Matheus <mtthew.providr@gmail.com>
2026-08-28 09:07:19 -0300

    exercise: solve 28-const-naming

---
+++ Study's/Module-01/28-const-naming.py
@@ -0,0 +1,9 @@
+#Crie três variáveis que representem constantes (ex: PI, MAX_USERS) seguindo a convenção de nomenclatura do Python para constantes, e imprima elas.
+
+PI = 3.14159
+MAX_USERS = 100
+MAX_LOGIN_ATTEMPTS = 3
+
+print(PI)
+print(MAX_USERS)
+print(MAX_LOGIN_ATTEMPTS)
