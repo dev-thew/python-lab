8885b41a84582e885c82b241a31def15eb17941b
Matheus <mtthew.providr@gmail.com>
2026-08-28 08:34:58 -0300

    fix (exercise): Translate 08-division-types and solve better

--- Study's/Module-01/08-division-types.py
+++ Study's/Module-01/08-division-types.py
@@ -1,7 +1,8 @@
-#Divide two integers using / and then using //. Print both results and explain the difference in a comment.
+#Divida dois números inteiros usando / e depois usando //. Imprima os dois resultados e explique a diferença em um comentário.
 
 number1 = 9
 number2 = 3
 
 print(number1 / number2)
 print(number1 // number2)
+print("A diferença é que a divisão com / retorna um float, enquanto a divisão com // retorna um inteiro.")
