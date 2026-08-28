931b3cb15782c6327afda42ea61a99a4e5176753
Matheus <mtthew.providr@gmail.com>
2026-08-28 09:18:42 -0300

    exercise: solve 32-variable-type-juggling

---
+++ Study's/Module-01/32-variable-type-juggling.py
@@ -0,0 +1,6 @@
+#Crie uma variável guardando um número como texto (ex: "42"), converta para int, faça uma conta com ela, depois converta o resultado de volta para str e imprima concatenado com texto.
+
+number = input("Enter a Number: ")
+result = int(number) + 8
+
+print(f"Result is {str(result)}")
