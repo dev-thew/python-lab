8d3a8615bdefc573865d09890a49c2c7ed53bbf0
Matheus <mtthew.providr@gmail.com>
2026-08-28 08:39:32 -0300

    exercise: solve 20-temperature-range

---
+++ Study's/Module-01/20-temperature-range.py
@@ -0,0 +1,6 @@
+#Dadas duas variáveis de temperatura (today e yesterday), imprima o resultado de comparar se hoje está mais quente usando >, sem usar if — apenas imprima o booleano direto.
+
+today = 30
+yesterday = 25
+
+print(today > yesterday)
