f903d1641767af8e0bbb108cee37a84b12f9a49f
Matheus <mtthew.providr@gmail.com>
2026-08-29 13:12:42 -0300

    fix (challenges): New resolution for 04-time-in-seconds

--- Study's/Module-01/Challenges/04-time-in-seconds.py
+++ Study's/Module-01/Challenges/04-time-in-seconds.py
@@ -4,8 +4,8 @@ hours = int(input("Enter the number of hours: "))
 minutes = int(input("Enter the number of minutes: "))
 seconds = int(input("Enter the number of seconds: "))
 
-convertion = hours * 60
-convertion = (convertion + minutes) * 60
-convertion = convertion + seconds
+convertion = hours * 3600
+convertion += minutes * 60
+convertion += seconds
 
 print(f"Result the convertion for seconds: {convertion}")
