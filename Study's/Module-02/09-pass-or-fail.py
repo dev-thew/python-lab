0f4891d977b83bc5c4d24ea29e706d2912318cf0
Matheus <mtthew.providr@gmail.com>
2026-09-01 08:07:43 -0300

    exercises: solve 09-pass-of-fail

---
+++ Study's/Module-02/09-pass-or-fail.py
@@ -0,0 +1,6 @@
+#Peça a média final de um aluno e informe se ele foi aprovado (média >= 6) ou reprovado.
+
+final_grade = float(input("Enter the student's final grade: "))
+
+if final_grade >= 6:
+    print(f"The student is approved with a final grade of {final_grade}.")
