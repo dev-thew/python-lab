3433cab52f850bf646e08e6542d0a99712711a99
Matheus <mtthew.providr@gmail.com>
2026-09-01 08:27:48 -0300

    exercises: solve 19-season-by-month

---
+++ Study's/Module-02/19-season-by-month.py
@@ -0,0 +1,30 @@
+#Peça o número do mês (1–12) e informe a estação do ano (considere o hemisfério sul: Verão = Dez/Jan/Fev, Outono = Mar/Abr/Mai, Inverno = Jun/Jul/Ago, Primavera = Set/Out/Nov).
+
+month_number = int(input("Enter the month number (1-12): "))
+
+if month_number == 12:
+    print("The season is Summer.")
+elif month_number == 1:
+    print("The season is Summer.")
+elif month_number == 2:
+    print("The season is Summer.")
+elif month_number == 3:
+    print("The season is Autumn.")
+elif month_number == 4:
+    print("The season is Autumn.")
+elif month_number == 5:
+    print("The season is Autumn.")
+elif month_number == 6:
+    print("The season is Winter.")
+elif month_number == 7:
+    print("The season is Winter.")
+elif month_number == 8:
+    print("The season is Winter.")
+elif month_number == 9:
+    print("The season is Spring.")
+elif month_number == 10:
+    print("The season is Spring.")
+elif month_number == 11:
+    print("The season is Spring.")
+else:
+    print("Error: Invalid month number. Please enter a number from 1 to 12.")
