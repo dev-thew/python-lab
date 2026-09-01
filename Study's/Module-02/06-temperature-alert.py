394555c5345e5bc95438e2c1fccf9614d1d292b5
Matheus <mtthew.providr@gmail.com>
2026-09-01 08:02:54 -0300

    exercises: solve 06-temperature-alert

---
+++ Study's/Module-02/06-temperature-alert.py
@@ -0,0 +1,6 @@
+#Peça a temperatura atual e exiba um alerta apenas se ela for maior que 35 (graus Celsius).
+
+temperature = float(input("Enter the current temperature in Celsius: "))
+
+if temperature > 35:
+     print("Alert: The temperature is above 35 degrees Celsius!")
