44eaedeb34db0c58c6716869b01e46f3be7a3d17
dev-thew <mtthew.providr@gmail.com>
2026-09-02 08:35:08 -0300

    exercises: solve 25-leap-year

--- Study's/Module-02/25-leap-year.py
+++ Study's/Module-02/25-leap-year.py
@@ -1,1 +1,9 @@
 #Peça um ano e informe se é bissexto. Regra: divisível por 4 E (não divisível por 100 OU divisível por 400).
+#dica: combine and/or numa única condição
+
+year = int(input("Enter a year:"))
+
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    print(f"Year: {year}\nIt is a leap year")
+else:
+    print(f"Year: {year}\nIt is not a leap year")
